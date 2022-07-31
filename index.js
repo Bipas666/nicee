2d48ba2c5ae2657efc53c695d51e5b9ff20cfc93
Bipas666 <koiralabipas@gmail.com>
2022-07-31 18:36:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+
+
+//for middleware
+require("./database/db");
+app.use("/uploads", express.static("uploads"));
+app.use(express.json());
+const userRoute = require("./routes/user");
+app.use("/user", userRoute);
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
+const blogRoute = require("./routes/blogpost");
+app.use("/blogPost", blogRoute);
+
+data = {
+  msg: "Welcome on DevStack Blog App development YouTube video series",
+  info: "This is a root endpoint",
+  Working: "Documentations of other endpoints will be release soon :)",
+  request:
+    "Hey if you did'nt subscribed my YouTube channle please subscribe it",
+};
+
+app.route("/").get((req, res) => res.json(data));
+
+app.listen('180');
